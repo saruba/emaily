f11d279fc083ee7bb11f696b34a61ea1a2afbc8c
Salvador Marí <saruba@gmail.com>
2018-05-09 14:15:50 +0200

    build for heroku

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
@@ -31,7 +32,6 @@ billingRoutes(app);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client', 'build')));
-  const path = require('path');
   app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
