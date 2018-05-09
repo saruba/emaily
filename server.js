6e1af531fd7b1fb7995669641a8281ecddc29fe2
Salvador Marí <saruba@gmail.com>
2018-05-09 14:12:31 +0200

    build for heroku

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ authRoutes(app);
 billingRoutes(app);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
   const path = require('path');
   app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
